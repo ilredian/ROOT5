a7c9d8b9ca2709d9aba4f2004fa63bd0afca8c6a
aqua <ilredian@naver.com>
2015-11-27 10:22:11 +0900

    컨트롤러 이동 로그 추가

--- src/controllers/RegistrationController.java
+++ src/controllers/RegistrationController.java
@@ -17,31 +17,37 @@ public class RegistrationController {
 	
 	@RequestMapping(value="game.go" , method=RequestMethod.GET)
 	public String game(){
+		System.out.println("게임 피해 등록 이동");
 		return "registration.game";
 	}
 	
 	@RequestMapping(value="game.go" , method=RequestMethod.POST)
 	public String game(String string){
+		System.out.println("게임 피해 DB 등록");
 		return main;
 	}
 	
 	@RequestMapping(value="trade.go" , method=RequestMethod.GET)
 	public String trade(){
+		System.out.println("거래 피해 등록 이동");
 		return "registration.trade";
 	}
 	
 	@RequestMapping(value="trade.go" , method=RequestMethod.POST)
 	public String trade(String string){
+		System.out.println("거래 피해 DB 등록");
 		return main;
 	}
 	
 	@RequestMapping(value="manner.go" , method=RequestMethod.GET)
 	public String manner(){
+		System.out.println("비매너 피해 등록 이동");
 		return "registration.manner";
 	}
 	
 	@RequestMapping(value="manner.go" , method=RequestMethod.POST)
 	public String manner(String string){
+		System.out.println("비매너 피해 DB 등록");
 		return main;
 	}
 }
