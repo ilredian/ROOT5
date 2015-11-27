9f1ea7f68a657e86ca76a44658a7ec640206a5e5
aqua <ilredian@naver.com>
2015-11-27 15:57:47 +0900

    한글깨짐 삭제2

--- src/controllers/RegistrationController.java
+++ src/controllers/RegistrationController.java
@@ -7,47 +7,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RegistrationController {
 	
-	private String main = "home.index"; //메인화면 이동
+	private String main = "home.index";
 
 	@RequestMapping("registration.go")
 	public String registration(){
-		System.out.println("피해 진술 이동");
 		return "registration.registration";
 	}
 	
 	@RequestMapping(value="game.go" , method=RequestMethod.GET)
 	public String game(){
-		System.out.println("게임 피해 등록 이동");
 		return "registration.game";
 	}
 	
 	@RequestMapping(value="game.go" , method=RequestMethod.POST)
 	public String game(String string){
-		System.out.println("게임 피해 DB 등록");
 		return main;
 	}
 	
 	@RequestMapping(value="trade.go" , method=RequestMethod.GET)
 	public String trade(){
-		System.out.println("거래 피해 등록 이동");
 		return "registration.trade";
 	}
 	
 	@RequestMapping(value="trade.go" , method=RequestMethod.POST)
 	public String trade(String string){
-		System.out.println("거래 피해 DB 등록");
 		return main;
 	}
 	
 	@RequestMapping(value="manner.go" , method=RequestMethod.GET)
 	public String manner(){
-		System.out.println("비매너 피해 등록 이동");
 		return "registration.manner";
 	}
 	
 	@RequestMapping(value="manner.go" , method=RequestMethod.POST)
 	public String manner(String string){
-		System.out.println("비매너 피해 DB 등록");
 		return main;
 	}
 }
