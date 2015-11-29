408cdae2b7ea3aa8c9b9be83b5033b6abf79ba2b
aqua <ilredian@naver.com>
2015-11-29 17:41:19 +0900

    CheatDAO 내용 추가

--- src/DAO/CheatDAO.java
+++ src/DAO/CheatDAO.java
@@ -1,5 +1,23 @@
 package DAO;
 
+import java.util.List;
+
+import DTO.CheatDTO;
+
 public interface CheatDAO {
 
+	//모든 사기종류 카테고리 가져오기
+	public List<CheatDTO> getAllCheat() throws Exception;
+	
+	//하나의 사기종류 카테고리 가져오기
+	public CheatDTO getCheat(int cheatno) throws Exception;
+	
+	//사기 종류 추가하기
+	public int insert(CheatDTO cheatDTO) throws Exception;
+	
+	//사기 종류 수정하기
+	public int update(CheatDTO cheatDTO) throws Exception;
+	
+	//사기 종류 삭제하기
+	public int delete(int cheatno) throws Exception;
 }
