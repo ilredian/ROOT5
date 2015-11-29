87beabb426ef12d87ac143ea9ccb5de15dea78cb
aqua <ilredian@naver.com>
2015-11-29 16:45:20 +0900

    cheaterDAO 내용 추가

--- src/DAO/CheaterDAO.java
+++ src/DAO/CheaterDAO.java
@@ -1,5 +1,23 @@
 package DAO;
 
-public interface CheaterDAO {
+import java.util.List;
 
+import DTO.CheaterDTO;
+
+public interface CheaterDAO {
+	
+	//진술서 등록 정보 모두 불러오기
+	public List<CheaterDTO> getAllCheater() throws Exception;
+	
+	//진술서 등록 정보 특정 한개만 불러오기
+	public CheaterDTO getCheater() throws Exception;
+	
+	//진술서 등록하기
+	public int insert() throws Exception;
+	
+	//진술서 수정하기
+	public int update() throws Exception;
+	
+	//진술서 삭제하기
+	public int delete() throws Exception;
 }
