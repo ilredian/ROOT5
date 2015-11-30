2ed8be7d2998bfdaf6cb24eeeee0c0ee984a26b9
aqua <ilredian@naver.com>
2015-11-30 09:22:16 +0900

    cheaterDAO에서 pager 추가를 위한 param 수정

--- src/DAO/CheaterDAO.java
+++ src/DAO/CheaterDAO.java
@@ -13,10 +13,10 @@ public interface CheaterDAO {
 	public int getCheaterCount(String field, String query, int cheatno) throws Exception;
 	
 	//진술서 등록 정보 전체 중 특정하여 불러오기
-	public List<CheaterDTO> getAllCheater(int page, String field, String query) throws Exception;
+	public List<CheaterDTO> getAllCheater(int page, String field, String query, int pageSize) throws Exception;
 	
 	//진술서 등록 정보 카테고리별 특정하여 불러오기
-	public List<CheaterDTO> getSearchCheater(int page, String field, String query, int cheatno) throws Exception;
+	public List<CheaterDTO> getSearchCheater(int page, String field, String query, int cheatno, int pageSize) throws Exception;
 	
 	//진술서 등록 정보 특정 한개만 불러오기
 	public CheaterDTO getCheater(int stateno) throws Exception;
