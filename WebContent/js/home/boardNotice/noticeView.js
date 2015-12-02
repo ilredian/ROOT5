c94bbf03708d113e3eea2ea3d76df2d7f5a57c60
aqua <ilredian@naver.com>
2015-12-02 17:00:24 +0900

    공지사항 게시판 완료

---
+++ WebContent/js/home/boardNotice/noticeView.js
@@ -0,0 +1,42 @@
+$('#edit').click(function() {
+	var pg = $('#pageNo').val();
+	var bno = $('#boardNo').val();
+	location.replace("noticeEdit.go?pg=" + pg + "&bno=" + bno);
+});
+
+$('#delete').click(function() {
+	var bno = $('#boardNo').val();
+	location.replace("noticeDelete.go?bno=" + bno);
+});
+
+$('#list').click(function() {
+	var pg = $('#pageNo').val();
+	location.replace("noticeMain.go?pg=" + pg);
+});
+
+$('#noticeWrite').click(function(){
+	location.replace("noticeWrite.go");
+});
+
+$('.updateReply').click(function(data){
+	$('.updateReply').hide();
+	$('.updateReplyActive').hide();
+	
+	console.log(data);
+	var path1 = data.target.origin;
+	var path2 = data.target.pathname;
+	var path3 = data.target.search;
+	var cancel = path1 + path2 + path3;
+	var id = data.target.id;
+	var hide = data.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[3].childNodes[1];
+	var $div = $(data.target.parentNode);
+	
+	$div.empty();
+	$div.append("<a style='color:red;' href='"+cancel+"'>수정 취소</a>");
+
+	var $hide = $(hide);
+
+	var text = $hide.html();
+	$hide.empty();
+	$hide.append("<form action='"+id+"' method='post'><textarea rows='5' cols='1' style='width:100%;' type='text' name='content'>"+text+"</textarea><div style='float:right; margin:5px'><input type='submit' class='btn btn-primary' value='댓글 수정'></div></form>");
+});
--- src/controllers/BoardController.java
+++ src/controllers/BoardController.java
@@ -306,7 +306,7 @@ public class BoardController {
 
 	// 3. 자유게시판 글쓰기(화면만 뿌리기)
 	@RequestMapping(value = "freeWrite.go", method = RequestMethod.GET)
-	public String freeWrite(BoardFreeDTO boardDTO) {
+	public String freeWrite() {
 		System.out.println("자유게시판 글쓰기 창");
 		return "home.boardFree.freeWrite";
 	}
@@ -332,7 +332,7 @@ public class BoardController {
 			out.print(
 					"<script type='text/javascript'>alert('글을 등록하는데 실패하였습니다.'); location.replace('redirect:freeMain.go?cno=1');</script>");
 		}
-		return "redirect:freeMain.go?cno=1";
+		return "redirect:freeMain.go?pg=1";
 	}
 
 	// 5. 게시물 수정 (화면 (select)
@@ -379,7 +379,7 @@ public class BoardController {
 			BoardFreeDAO boardFreeDAO = sqlSession.getMapper(BoardFreeDAO.class);
 			boardFreeDAO.update(boardFreeDTO);
 			System.out.println("자유게시판 수정완료");
-			go = "redirect:freeMain.go?pg"+page;
+			go = "redirect:freeMain.go?pg="+page;
 		}else{
 			go = "redirect:index.go";
 		}
@@ -409,121 +409,211 @@ public class BoardController {
 	//////////// 공지게시판////////////
 	//////////// 공지게시판////////////
 
-	// 1. 공지게시판 메인(목록 리스트)
-	@RequestMapping("noticeMain.go")
-	public String noticeMain(@RequestParam(value = "pg", required = false, defaultValue = "1") int page, // 현재
-																											// 페이지
-																											// 번호
-			@RequestParam(value = "f", required = false, defaultValue = "title") String field, // 검색
-																								// 카테고리
-			@RequestParam(value = "q", required = false, defaultValue = "%%") String query, // 검색
-																							// 내용
-			@RequestParam(value = "ps", required = false, defaultValue = "10") int pageSize, // 한
-																								// 페이지에
-																								// 보여줄
-																								// 게시글
-																								// 갯수
-			Model model) throws Exception {
+	// 1. 공지 게시판 메인(목록리스트)
+		@RequestMapping("noticeMain.go") // 공지 게시판 메인- 목록보기 List
+		public String noticeMain(// get으로 들어오는 parameter값 선언 및 기본값 설정
+				@RequestParam(value = "pg", required = false, defaultValue = "1") int page, // 현재 페이지 번호
+				@RequestParam(value = "f", required = false, defaultValue = "title") String field, // 검색 카테고리
+				@RequestParam(value = "q", required = false, defaultValue = "%%") String query, // 검색 내용
+				@RequestParam(value = "ps", required = false, defaultValue = "10") int pageSize, // 한 페이지에 보여줄 게시글 갯수
+				HttpSession session,
+				Model model) throws Exception {
 
-		System.out.println(page + " / " + field + " / " + query);
+			// 로그 남기기
+			System.out.println(page + " / " + field + " / " + query);
 
-		BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+			BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
 
-		int pagerSize = 10;// 한 번에 보여줄 페이지 번호 갯수
-		String linkUrl = "freeMain.go";// 페이지번호를 누르면 이동할 경로
-		int boardCount = boardNoticeDAO.getCount(field, query);// 검색 결과에 따른 게시글
-																// 총 갯수
-		int start = (page - 1) * pageSize;
-		BoardPager pager = new BoardPager(boardCount, page, pageSize, pagerSize, linkUrl);
-
-		List<BoardNoticeDTO> list = boardNoticeDAO.getNotices(start, field, query, pageSize);
-		System.out.println("zzz");
-
-		model.addAttribute("list", list); // 자동 forward
-		System.out.println("xxx");
-
-		return "home.boardNotice.noticeMain";
-	}
-
-	// 2. 공지게시판 상세보기
-	@RequestMapping("noticeView.go")
-	public String noticeView(String seq , Model model) throws ClassNotFoundException, SQLException{
-	
-	  	 BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
-		 BoardNoticeDTO boardNoticeDTO = boardNoticeDAO.getNotice(seq);
-		 model.addAttribute("boardNoticeDTO", boardNoticeDTO); ///// DB 테이블 명--파라미터명 일치 여부 확인후 수정바람*****
-
-		return "home.boardNotice.noticeView";
-	}
-
-	// 3. 공지게시판 글쓰기(화면만 뿌리기)
-	@RequestMapping(value = "noticeWrite.go", method = RequestMethod.GET)
-	public String noticeWriteView(BoardNoticeDTO DTO) {
-		return "home.boardNotice.noticeWrite";
-	}
-	
-	//4. 공지게시판 글쓰기(실제 글 등록 -DB)
-	@RequestMapping(value="noticeWrite.go" , method=RequestMethod.POST)
-	public String noticeWrite(BoardNoticeDTO DTO, HttpServletRequest request ) throws ClassNotFoundException, SQLException{
-			System.out.println("실제 글 등록 처리"); 
+			int pagerSize = 10;// 한 번에 보여줄 페이지 번호 갯수
+			String linkUrl = "noticeMain.go";// 페이지번호를 누르면 이동할 경로
+			int boardCount = boardNoticeDAO.getCount(field, query);// 검색 결과에 따른 게시글 총
+																	// 갯수
+			int start = (page - 1) * pageSize;
+			BoardPager pager = new BoardPager(boardCount, page, pageSize, pagerSize, linkUrl);
 			
-		    System.out.println("n : " + DTO.getTitle());
-		    System.out.println("n : " + DTO.getContent());
-		    
-		    BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
-		    boardNoticeDAO.insert(DTO);
-		return "home.boardNotice.noticeMain";
-	}
+			List<BoardNoticeDTO> list= boardNoticeDAO.getNotices(start, field, query, pageSize);
+			
+			String email = ((MemberDTO)session.getAttribute("memberInfo")).getEmail();
 
-	/*
-	 * // 5. 게시물 수정
-	 * 
-	 * @RequestMapping(value = "noticeEdit.go", method = RequestMethod.GET)
-	 * public String noticeEdit(
-	 * 
-	 * @RequestParam("bno") int boardno, HttpSession session, Model model)
-	 * throws Exception {
-	 * 
-	 * //로그 남기기 System.out.println("게시물 수정 페이지로 이동");
-	 * 
-	 * //페이지 이동 변수 선언 String go = "";
-	 * 
-	 * BoardFreeDAO boardFreeDAO = sqlSession.getMapper(BoardFreeDAO.class);
-	 * 
-	 * int memberno =
-	 * ((MemberDTO)session.getAttribute("memberInfo")).getMemberno();
-	 * BoardFreeDTO boardFreeDTO = boardFreeDAO.getNotice(boardno); if(memberno
-	 * == boardFreeDTO.getMemberno()){ System.out.println("자유게시판 원본글 가져오기");
-	 * model.addAttribute("boardFreeDTO", boardFreeDTO); go =
-	 * "home.boardNotice.noticeEdit"; }else{ go = "redirect:index.go"; } return
-	 * go;
-	 * 
-	 * }
-	 */
-	// 5-1. 게시물 수정 (실제 처리(update)
-	@RequestMapping(value = "noticeEdit.go", method = RequestMethod.POST)
-	public String noticeEdit(BoardNoticeDTO boardDTO, HttpServletRequest request)
-			throws ClassNotFoundException, SQLException {
+			model.addAttribute("Email", email);
+			model.addAttribute("pager", pager);
+			model.addAttribute("boardCount", boardCount);
 
-		BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
-		boardNoticeDAO.update(boardDTO);
-		System.out.println("공지게시판 수정완료");
+			//메인 리스트에 댓글 숫자 출력
+			ReplyDAO replyDAO = sqlSession.getMapper(ReplyDAO.class);
+			for(int i=0; i < list.size(); i++){
+				list.get(i).setBoardReplyCount(replyDAO.getBoardReplyCount("content", query, list.get(i).getBoardno()));
+			}
+			model.addAttribute("list", list); // 자동 forward
+					
+			System.out.println("공지 게시판 메인 출력 완료");
+			
+			return "home.boardNotice.noticeMain";
+		}
 
-		return "home.boardNotice.noticeMain";
-	}
-	
-	//6. 게시물 삭제
-	@RequestMapping("noticeDelete.go")   
-	public String noticeDelete(String boardno) throws ClassNotFoundException, SQLException{
-		
-	    BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
-	    boardNoticeDAO.delete(boardno);
-	    
-	    System.out.println("공지게시판 삭제완료");
-	    
-	    return "home.boardNotice.noticeMain";
+		// 2. 공지 게시판 상세보기
+		@RequestMapping("noticeView.go") // 공지 게시판 상세보기 - 페이지
+		public String noticeView(
+				@RequestParam("bno") int boardno, // 게시글 번호
+				@RequestParam(value = "pg", required = false, defaultValue = "1") int page, // 현재 페이지 번호
+				@RequestParam(value = "f", required = false, defaultValue = "title") String field, // 검색 카테고리
+				@RequestParam(value = "q", required = false, defaultValue = "%%") String query, // 검색 내용
+				@RequestParam(value = "ps", required = false, defaultValue = "10") int pageSize, // 한 페이지에 보여줄 게시글 갯수
+				@RequestParam(value = "rpg", required = false, defaultValue = "1") int replypage, // 현재 페이지 번호
+				HttpSession session, 
+				Model model) throws Exception {
+			// 로그남기기
+			System.out.println("공지 게시판 상세보기 페이지 이동");
 
-	}
+			System.out.println("게시판 정보 가져오기");
+			BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+			BoardNoticeDTO boardNoticeDTO = boardNoticeDAO.getNotice(boardno);
+
+			//if (((MemberDTO) session.getAttribute("memberInfo")).getMemberno() != boardNoticeDTO.getMemberno()) {
+				System.out.println("조회수 증가");
+				boardNoticeDAO.updateCountno(boardno);
+				boardNoticeDTO.setCountno(boardNoticeDTO.getCountno() + 1);
+			//}
+			System.out.println("리플 정보 가져오기");
+			ReplyDAO replyDAO = sqlSession.getMapper(ReplyDAO.class);
+			int replycount = replyDAO.getBoardReplyCount("content", "%%", boardno);
+			
+			// 리플 페이지 처리
+			int rstart = (replypage - 1) * 10;
+			ReplyPager rpager = new ReplyPager(replycount, replypage, 10, 10, "noticeView.go", boardno, page);
+			
+			List<ReplyDTO> replyDTO = replyDAO.getBoardReply("content", "%%", boardno, rstart);
+			
+			model.addAttribute("boardNoticeDTO", boardNoticeDTO);
+			model.addAttribute("replycount", replycount);
+			model.addAttribute("rpager", rpager);
+			model.addAttribute("replyDTO", replyDTO);
+			
+			//상세보기 밑 리스트 뿌려주기
+			// 로그 남기기
+			System.out.println(page + " / " + field + " / " + query);
+
+			boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+
+			String linkUrl = "freeMain.go";// 페이지번호를 누르면 이동할 경로
+			int boardCount = boardNoticeDAO.getCount(field, query);// 검색 결과에 따른 게시글 총 갯수
+			int pagerSize = 10;// 한 번에 보여줄 페이지 번호 갯수
+			int start = (page - 1) * pageSize;
+			BoardPager pager = new BoardPager(boardCount, page, pageSize, pagerSize, linkUrl);
+					
+			List<BoardNoticeDTO> list= boardNoticeDAO.getNotices(start, field, query, pageSize);
+					
+			String email = ((MemberDTO)session.getAttribute("memberInfo")).getEmail();
+
+			model.addAttribute("Email", email);
+			model.addAttribute("pager", pager);
+			model.addAttribute("boardCount", boardCount);
+			
+			//메인 리스트에 댓글 숫자 출력
+			for(int i=0; i < list.size(); i++){
+				list.get(i).setBoardReplyCount(replyDAO.getBoardReplyCount("content", query, list.get(i).getBoardno()));
+			}
+			
+			model.addAttribute("list", list); // 자동 forward
+			
+			return "home.boardNotice.noticeView";
+		}
+
+		// 3. 공지 게시판 글쓰기(화면만 뿌리기)
+		@RequestMapping(value = "noticeWrite.go", method = RequestMethod.GET)
+		public String noticeWrite() {
+			System.out.println("공지 게시판 글쓰기 창");
+			return "home.boardNotice.noticeWrite";
+		}
+
+		// 4. 공지 게시판 글쓰기(실제 글 등록 -DB)
+		@RequestMapping(value = "noticeWrite.go", method = RequestMethod.POST)
+		public String freeWrite(BoardNoticeDTO boardNoticeDTO, HttpSession session, HttpServletResponse response,
+				HttpServletRequest request) throws Exception {
+			System.out.println("실제 글 등록 처리");
+
+			response.setContentType("text/html;charset=UTF-8");
+			out = response.getWriter();
+
+			BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+			int result = boardNoticeDAO.insert(boardNoticeDTO);
+
+			if (result != 0) {
+				System.out.println("공지 게시판 글쓰기 완료");
+			} else {
+				System.out.println("공지 게시판 글쓰기 등록 실패");
+			}
+			return "redirect:noticeMain.go?pg=1";
+		}
+
+		// 5. 게시물 수정 (화면 (select)
+		@RequestMapping(value = "noticeEdit.go", method = RequestMethod.GET)
+		public String noticeEdit(@RequestParam("bno") int boardno, HttpSession session, Model model) throws Exception {
+
+			// 로그 남기기
+			System.out.println("게시물 수정 페이지로 이동");
+
+			// 페이지 이동 변수 선언
+			String go = "";
+
+			BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+
+//			int memberno = ((MemberDTO) session.getAttribute("memberInfo")).getMemberno();
+			BoardNoticeDTO boardNoticeDTO = boardNoticeDAO.getNotice(boardno);
+			//if (memberno == boardNoticeDTO.getMemberno()) {
+				System.out.println("공지게시판 원본글 가져오기");
+				model.addAttribute("boardNoticeDTO", boardNoticeDTO);
+				go = "home.boardNotice.noticeEdit";
+/*			} else {
+				go = "redirect:index.go";
+			}*/
+			return go;
+		}
+
+		// 5-1. 게시물 수정 (실제 처리(update)
+		@RequestMapping(value = "noticeEdit.go", method = RequestMethod.POST)
+		public String noticeEdit(
+								@RequestParam("pg") int page,
+								BoardNoticeDTO boardNoticeDTO,
+								HttpSession session,
+								HttpServletRequest request) throws Exception {
+
+			//로그 남기기
+			System.out.println("게시물 수정 작업 시작");
+			
+			//페이지 이동 변수 선언
+			String go = "";
+			
+//			int memberno = ((MemberDTO)session.getAttribute("memberInfo")).getMemberno();
+			
+			//if(memberno == boardFreeDTO.getMemberno()){
+				BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+				boardNoticeDAO.update(boardNoticeDTO);
+				System.out.println("공지 게시판 수정완료");
+				go = "redirect:noticeMain.go?pg="+page;
+/*			}else{
+				go = "redirect:index.go";
+			}*/
+			return go;
+		}
+
+		// 6. 게시물 삭제
+		@RequestMapping("noticeDelete.go")
+		public String noticeDelete(@RequestParam("bno") int boardno, HttpSession session) throws Exception {
+
+			BoardNoticeDAO boardNoticeDAO = sqlSession.getMapper(BoardNoticeDAO.class);
+
+//			int memberno = boardNoticeDAO.getNotice(boardno).getMemberno();
+//			if (((MemberDTO) session.getAttribute("memberInfo")).getMemberno() == memberno) {
+				boardNoticeDAO.updateActive(boardno);
+				System.out.println("공지 게시판 게시물 삭제(active 수정)완료");
+/*			} else {
+				System.out.println("공지 게시판 게시물을 삭제(active 수정)할 수 없습니다.");
+			}*/
+
+			return "redirect:noticeMain.go?pg=1";
+
+		}
 	
 	///////////////////////////////////
 	///////////////////////////////////
