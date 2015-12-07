f134277430e1897a39e06028a3f1e860092b64b9
aqua <ilredian@naver.com>
2015-12-07 13:14:46 +0900

    admin js 통합 추가 및 수정

--- WebContent/js/admin/adminBoardFree.js
+++ WebContent/js/admin/adminBoardFree.js
@@ -1,17 +1,26 @@
-$('#update').click(function(){
-	if(confirm('선택하신 항목을 수정하시겠습니까?')){
-		location.replace('');
+$('.delete').click(function() {
+	if (confirm('선택하신 항목을 삭제하시겠습니까?')) {
+		location.replace("'" + $(this).attr("id") + "'");
 	}
 });
 
-$('#delete').click(function(){
-	if(confirm('선택하신 항목을 삭제하시겠습니까?')){
-		location.replace('');
-	}
-});
+function setPreviewBox(e) {
+	var e = e || window.event;
+	document.getElementById('preview').style.left = e.clientX
+			+ document.body.scrollLeft + 20 + 'px'; // 마우스 
+	document.getElementById('preview').style.top = e.clientY
+			+ document.body.scrollTop + 'px'; // 포인트에 위치 
+}
 
-$('#insert').click(function(){
-	if(confirm('이 항목을 추가하시겠습니까?')){
-		location.replace('');
-	}
-});
+function showPreview(content) {
+	var text;
+	text = '<table cellpadding="5" bgcolor="#ffffff" style="font-size:9pt;color:#005F8B;filter:alpha(opacity=90); border-width:1; border-color:#3291BD; border-style:solid;">';
+	text += '<tr><td>' + content + '</td></tr></table>';
+	document.getElementById('preview').innerHTML = text;
+	document.getElementById('preview').style.visibility = 'visible';
+}
+
+function hidePreview() {
+	document.getElementById('preview').innerHTML = '';
+	document.getElementById('preview').style.visibility = 'hidden';
+}
--- WebContent/js/admin/adminComment.js
+++ WebContent/js/admin/adminComment.js
@@ -1,17 +1,5 @@
-$('#update').click(function(){
-	if(confirm('선택하신 항목을 수정하시겠습니까?')){
-		location.replace('');
-	}
-});
-
-$('#delete').click(function(){
-	if(confirm('선택하신 항목을 삭제하시겠습니까?')){
-		location.replace('');
-	}
-});
-
-$('#insert').click(function(){
-	if(confirm('이 항목을 추가하시겠습니까?')){
-		location.replace('');
+$('.delete').click(function() {
+	if (confirm('선택하신 항목을 삭제하시겠습니까?')) {
+		location.replace("'" + $(this).attr("id") + "'");
 	}
 });
