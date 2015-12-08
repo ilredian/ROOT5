25720645ba9386231165009ac33f6b15aa65e41c
inyoung <jkikss@gmail.com>
2015-12-08 10:42:28 +0900

    new.gif 추가_ 멤버 인포 컨트롤러_

--- src/controllers/MemberInfoController.java
+++ src/controllers/MemberInfoController.java
@@ -146,6 +146,8 @@ public class MemberInfoController {
 		MemberDAO memberDAO = sqlSession.getMapper(MemberDAO.class);
 		String originpwd = ((MemberDTO)session.getAttribute("memberInfo")).getPassword();
 		int memberno = ((MemberDTO)session.getAttribute("memberInfo")).getMemberno();
+		
+		
 		System.out.println(originpwd);
 		System.out.println(memberno);
 		
