4f4f07aa81411c58b6ea0e220c1cafba89bd7ccd
rk4142 <rk4142@naver.com>
2015-12-15 21:59:15 +0900

    마이페이지

--- WebContent/WEB-INF/views/memberInfo/inc/visual.jsp
+++ WebContent/WEB-INF/views/memberInfo/inc/visual.jsp
@@ -5,7 +5,7 @@
 <ul class="nav nav-tabs">
 	<li id="memberBoard"><a href="memberBoard.go?mno=1">게시물</a></li>
 	<li id="memberStatement"><a href="memberStatement.go?mno=2">진술서</a></li>
-	<li id="memberStatement2"><a href="memberStatement2.go?mno=3">진술서2</a></li>
+	<li id="memberStatement2"><a href="memberStatement2.go?mno=3">사건처리현황</a></li>
 	<li id="memberComment"><a href="memberComment.go?mno=4">댓글</a></li>
 	<li id="memberConnect"><a href="memberConnect.go?mno=5">접속기록</a></li>
 	<li id="memberModify"><a href="memberModify.go?mno=6">정보수정</a></li>
--- WebContent/js/memberInfo/memberInfo.js
+++ WebContent/js/memberInfo/memberInfo.js
@@ -7,28 +7,20 @@ $(function() {
 	var cno = $('#mno').attr("value");
 	if(cno == 1){
 		$('#memberBoard').attr("class", "active");
-		$('#statementText').text("게시물");
 	}else if(cno == 2){
-		$('#memberComment').attr("class", "active");
-		$('#statementText').text("댓글");
-	}else if(cno == 3){
-		$('#memberConnect').attr("class", "active");
-		$('#statementText').text("접속기록");
-	}else if(cno == 4){
-		$('#memberModify').attr("class", "active");
-		$('#statementText').text("정보수정");
-	}else if(cno == 5){
-		$('#memberPwdChange').attr("class", "active");
-		$('#statementText').text("비번변경");
-	}else if(cno == 6){
-		$('#memberWithdrawal').attr("class", "active");
-		$('#statementText').text("회원탈퇴");
-	}else if(cno == 7){
 		$('#memberStatement').attr("class", "active");
-		$('#statementText').text("진술서");
-	}else{
+	}else if(cno == 3){
 		$('#memberStatement2').attr("class", "active");
-		$('#statementText').text("진술서");
+	}else if(cno == 4){
+		$('#memberComment').attr("class", "active");
+	}else if(cno == 5){
+		$('#memberConnect').attr("class", "active");
+	}else if(cno == 6){
+		$('#memberModify').attr("class", "active");
+	}else if(cno == 7){
+		$('#memberPwdChange').attr("class", "active");
+	}else{
+		$('#memberWithdrawal').attr("class", "active");
 	}
 });
 
