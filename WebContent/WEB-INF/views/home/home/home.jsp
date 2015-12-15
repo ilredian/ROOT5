870abb7cf428abbd22af50866faf7cf6d3cb49ca
lee <lghd1010@naver.com>
2015-12-15 15:00:42 +0900

    no message

--- WebContent/WEB-INF/views/home/home/home.jsp
+++ WebContent/WEB-INF/views/home/home/home.jsp
@@ -123,7 +123,7 @@
 							</div>
 						</div>
 					</div>
-					<a href="#">
+					<a href="PhotoMain.go">
 						<div class="panel-footer">
 							<span class="pull-left">View Details</span> <span
 								class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
--- src/controllers/BoardControllerPhoto.java
+++ src/controllers/BoardControllerPhoto.java
@@ -1,5 +1,24 @@
 package controllers;
 
-public class BoardControllerPhoto {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class BoardControllerPhoto {
+   
+   @RequestMapping(value="PhotoMain.go", method = RequestMethod.GET)
+   public String freeMain(){
+      
+      System.out.println("사진게시판 이동");
+      
+      return "home.boardPhoto.photoMain";
+   }
+   @RequestMapping(value="PhotoWrite.go", method = RequestMethod.GET)
+   public String photowrite(){
+      
+      System.out.println("사진글 등록");
+      
+      return "home.boardPhoto.photowrite";
+   }
 }
